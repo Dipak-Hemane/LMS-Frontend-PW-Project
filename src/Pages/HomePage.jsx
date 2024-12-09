b905349c4ce54586f2d4c9fb68fc4bb6b300489f
Dipak-Hemane <dipakhemane123@gmail.com>
2024-12-09 11:50:06 +0530

    second commit for page component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import HomeLayout from '../Layouts/HomeLayout'
 import { Link } from 'react-router-dom';
-import Images from '..assets/Images/homePageMainImage.png';
+import HomePageImage from '..assets/Images/homePageMainImage.png';
 function HomePage() {
     return (
         <HomeLayout>
@@ -32,7 +32,7 @@ function HomePage() {
                 </div>
 
                 <div className='w-1/2 flex items-center justify-center'>
-                    <img alt='homepage image' src="" />
+                    <img alt='homepage image' src={HomePageImage} />
                 </div>
 
             </div>
