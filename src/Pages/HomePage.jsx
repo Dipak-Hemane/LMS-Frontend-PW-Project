b855439887133df72f7859c8f7e2131226ed4227
Dipak-Hemane <dipakhemane123@gmail.com>
2024-12-23 23:41:37 +0530

    Added course creation by admin

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import HomePageImage from '../assets/Images/homePageMainImage.png';
 function HomePage() {
     return (
         <HomeLayout>
-            <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-{90vh}">
+            <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 mb-20 h-{90vh}"> 
                 <div className="w-1/2 space-y-6">
                     <h1 className="text-5xl font-semibold">
                         Find out best
--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import toast from "react-hot-toast"
 import axiosInstance from "../../Helpers/axioswInstance"
 
+
 const initialState = {
     courseData: []
 }
@@ -16,6 +17,29 @@ export const getAllCourses = createAsyncThunk("/course/get", async () => {
         });
 
         return (await response).data.courses;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+});
+
+export const createNewCourse = createAsyncThunk("/course/create", async (data) => {
+    try {
+        let formData = new FormData();
+        formData.append("title", data?.title);
+        formData.append("description", data?.description);
+        formData.append("category", data?.category);
+        formData.append("createdBy", data?.createdBy);
+        formData.append("thumbnail", data?.title);
+
+        const response = axiosInstance.post("/courses", formData);
+        toast.promise(response, {
+            loading: "Creating new course",
+            success: "Course created successfully",
+            error: "Failed to create course"
+        });
+
+        return (await response).data;
+
     } catch (error) {
         toast.error(error?.response?.data?.message);
     }
