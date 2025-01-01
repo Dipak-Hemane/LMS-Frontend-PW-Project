38ce0715a057adcb30c9cef96cadc69faa983300
Dipak-Hemane <dipakhemane123@gmail.com>
2025-01-01 11:22:08 +0530

    Fix: ADDED add lecture button for admin user

--- src/Pages/Dashboard/Displaylectures.jsx
+++ src/Pages/Dashboard/Displaylectures.jsx
@@ -32,7 +32,8 @@ function Displaylectures() {
                     Course Name: {state.title}
                 </div>
 
-                {lectures && lectures.length > 0 && <div className="flex justify-center gap-10 w-full">
+                {(lectures && lectures.length > 0 ) ? 
+                (<div className="flex justify-center gap-10 w-full">
                     {/* left section for playing videos and displaying course details to admin */}
                     <div className="space-y-5 w-[28rem] p-2 rounded-lg shodow-[0_0_10px_black]">
                        <video  
@@ -92,7 +93,13 @@ function Displaylectures() {
                             }
                         </ul>
                     </div>
-                </div>}
+                </div>) : (
+                    role && role == "ADMIN" && (
+                        <button onClick={() => navigate("/course/addlecture", {state: {...state}})} className="btn-primary px-2 py-1 rounded-md font-semibold text-sm">
+                            Add new lecture
+                        </button>
+                    )
+                )}
             </div>
         </HomeLayout>
     )
